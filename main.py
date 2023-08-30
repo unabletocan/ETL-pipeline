72c3ff914f2927afafb9d6a3559fa22e3480bbbd
unabletocan <ccollinsmoollan@gmail.com>
2023-08-30 15:22:31 +0200

    Adding the main script that executed all the ETL steps

---
+++ main.py
@@ -0,0 +1,33 @@
+# The main script that executes all the steps:
+
+import os
+
+# you need this library to read environment
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from src.extract import extract_transactional_data
+from src.transform import remove_duplicates
+from src.load_data_to_s3 import df_to_s3
+
+# import variables from .env file
+dbname = os.getenv("dbname")
+host = os.getenv("host")
+port = os.getenv("port")
+user = os.getenv("user")
+password = os.getenv("password")
+aws_access_key_id = os.getenv("aws_access_key_id")
+aws_secret_access_key_id = os.getenv("aws_secret_access_key_id")
+
+# make a connection to redshift and extract transactional data with transformation tasks
+print("Extracting and transforming data from the warehouse")
+online_trans_cleaned = extract_transactional_data(dbname, host, port, user, password)
+
+online_trans_cleaned = remove_duplicates(online_trans_cleaned)
+print("the size of data after removing duplicates", online_trans_cleaned.shape)
+
+# load the data to s3
+key = 'etl_pipeline/cm_online_transactions_v2.pkl'
+s3_bucket = 'july-bootcamp'
+df_to_s3(online_trans_cleaned, key, s3_bucket, aws_access_key_id, aws_secret_access_key_id)
